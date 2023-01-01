74ec207d47a50bbbf46c27473e03c22988621353
Алексей Ермаков <lexlaeda@Aleksejs-MacBook-Pro.local>
2023-01-01 16:06:05 +0400

    init commit

---
+++ src/main/java/com/geo/easypoint/EasypointApplication.java
@@ -0,0 +1,13 @@
+package com.geo.easypoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasypointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasypointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geo/easypoint/EasypointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geo.easypoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasypointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
